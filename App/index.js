70e936457f32f976290a887445175518f1f54634
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2017-04-09 19:08:19 +0100

    init

---
+++ App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Container, Header, Title, Button, Left, Right, Body, Icon } from 'native-base';
+
+const App = () => (
+  <View>
+    <Text>Eyo</Text>
+  </View>
+);
+
+export default App;
